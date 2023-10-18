875c7ed9406e32207f99a97389046d8a6d5eb5a8
Pablo Souza <pvrsouza@gmail.com>
2023-10-18 13:27:40 -0300

    feat: inclusao mecanica de lock distribuido para evitar race condition entre instancias diferentes

---
+++ src/main/java/br/com/desafiovr/miniautorizador/config/LockConfig.java
@@ -0,0 +1,24 @@
+package br.com.desafiovr.miniautorizador.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.integration.jdbc.lock.DefaultLockRepository;
+import org.springframework.integration.jdbc.lock.JdbcLockRegistry;
+import org.springframework.integration.jdbc.lock.LockRepository;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+
+@Component
+public class LockConfig {
+
+    @Bean
+    public DefaultLockRepository defaultLockRepository(DataSource dataSource){
+        return new DefaultLockRepository(dataSource);
+    }
+
+    @Bean
+    public JdbcLockRegistry jdbcLockRegistry(LockRepository lockRepository){
+        return new JdbcLockRegistry(lockRepository);
+    }
+
+}
--- src/main/java/br/com/desafiovr/miniautorizador/service/transacoes/TransacaoServiceImpl.java
+++ src/main/java/br/com/desafiovr/miniautorizador/service/transacoes/TransacaoServiceImpl.java
@@ -7,9 +7,11 @@ import br.com.desafiovr.miniautorizador.model.entity.Cartao;
 import br.com.desafiovr.miniautorizador.service.CartaoService;
 import br.com.desafiovr.miniautorizador.service.transacoes.processador.CadeiaValidacoesTransacao;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.integration.support.locks.LockRegistry;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.util.concurrent.locks.Lock;
 
 @Service
 @Slf4j
@@ -19,22 +21,31 @@ public class TransacaoServiceImpl implements TransacaoService {
 
     private final CartaoService cartaoService;
 
-    public TransacaoServiceImpl(CadeiaValidacoesTransacao cadeiaValidacoesTransacao, CartaoService cartaoService) {
+    private final LockRegistry lockRegistry;
+
+    public TransacaoServiceImpl(CadeiaValidacoesTransacao cadeiaValidacoesTransacao, CartaoService cartaoService, LockRegistry lockRegistry) {
         this.cadeiaValidacoesTransacao = cadeiaValidacoesTransacao;
         this.cartaoService = cartaoService;
+        this.lockRegistry = lockRegistry;
     }
 
     @Override
     public void registra(TransacaoRequestDto transacao) throws ValidacaoTransacaoException, CartaoNotFoundException {
-        //TODO: tentar fazer algum tipo de bloqueio da transacao com um lock centalizado na base que resolva race condition entre aplicações diferentes
-        //TODO: O lock deve ser feito aqui, antes de iniciar a cadeia de validações pois a cadeia de validações pode demorar um pouco e o saldo pode mudar entre a validao e a atualização do saldo
-        log.info("Transação INICIADA - cartão {}", transacao.getNumeroCartao());
-        this.cadeiaValidacoesTransacao.execute(transacao);
+        var lock = lockRegistry.obtain(transacao.getNumeroCartao());
+        boolean lockAdiquirido = lock.tryLock();
+        if (lockAdiquirido) {
+            try {
+                log.info("Transação INICIADA - cartão {}", transacao.getNumeroCartao());
+                this.cadeiaValidacoesTransacao.execute(transacao);
 
-        log.info("Atualizando saldo - cartão {}", transacao.getNumeroCartao());
-        this.atualizaSaldo(transacao);
+                log.info("Atualizando saldo - cartão {}", transacao.getNumeroCartao());
+                this.atualizaSaldo(transacao);
 
-        log.info("Transação REALIZADA com sucesso - cartão {}", transacao.getNumeroCartao());
+                log.info("Transação REALIZADA com sucesso - cartão {}", transacao.getNumeroCartao());
+            } finally {
+                lock.unlock();
+            }
+        }
     }
 
     private void atualizaSaldo(TransacaoRequestDto transacaoRequestDto) throws CartaoNotFoundException {
@@ -46,6 +57,6 @@ public class TransacaoServiceImpl implements TransacaoService {
 
         this.cartaoService.atualizaSaldo(cartao, resultado);
 
-        log.info("Saldo atualizado com sucesso no cartao {}. Saldo anterior [{}] Saldo atual [{}]",numeroCartao, saldoAnterior, cartao.getSaldo());
+        log.info("Saldo atualizado com sucesso no cartao {}. Saldo anterior [{}] Saldo atual [{}]", numeroCartao, saldoAnterior, cartao.getSaldo());
     }
 }
